0e8ae81189f9078912f23984f42acd6907c32dc9
Volodymyr <vladimir.savchenko@gmail.com>
2018-01-17 16:49:11 +0100

    response rebinning

--- useresponse.py
+++ useresponse.py
@@ -7,6 +7,7 @@ import numpy as np
 import ast
 import ddosa
 import dataanalysis.core as da
+import pilton
 
 import findic
 
@@ -27,9 +28,6 @@ class FindResponse(ddosa.DataAnalysis):
 class FindICEBDS(findic.FindICIndexEntry):
     ds="ISGR-EBDS-MOD"
 
-    @property
-    def ebds_mod_fn(self):
-        return self.member_location
 
 class CompressEBins(ddosa.DataAnalysis):
     input_ic_ebds=FindICEBDS
@@ -62,6 +60,41 @@ class CompressEBins(ddosa.DataAnalysis):
         f.writeto(self.ebds_mod_fn,overwrite=True)
 
 
+class RebinResponse(ddosa.DataAnalysis):
+    input_rsp = FindResponse
+    input_ic_ebds = FindICEBDS
+    input_ebins=CompressEBins
+
+    def main(self):
+        new_e = fits.open(self.input_ebins.ebds_mod_fn)[1]
+        new_bins = zip(new_e.data['E_MIN'], new_e.data['E_MAX'])
+        print("new bins:",new_bins)
+
+        orig_e = fits.open(self.input_ic_ebds.member_location)[1]
+        orig_bins = zip(orig_e.data['E_MIN'], orig_e.data['E_MAX'])
+        print("original bins",orig_bins)
+
+        rmf_e=fits.open(self.input_rsp.rmf_path)[1]
+
+        orig_rsp_fn="original_rsp_assembled.fits"
+        fits.HDUList([fits.PrimaryHDU(),orig_e,rmf_e]).writeto(orig_rsp_fn,overwrite=True)
+
+        ebins_compress_fn="ebins.txt"
+        with open(ebins_compress_fn,"w") as f:
+            for oi,(e1,e2) in enumerate(new_bins):
+                i1 = list(orig_e.data['E_MIN']).index(e1)
+                i2 = list(orig_e.data['E_MAX']).index(e2)
+                print(oi,i1,i2,e1,e2)
+                f.write("%i %i %i\n"%(i1,i2,i2-i1+1))
+
+        new_rsp_fn="rsp_rebinned.fits"
+
+        ht = pilton.heatool("rbnrmf")
+        ht['infile']=orig_rsp_fn
+        ht['binfile']=ebins_compress_fn
+        ht['outfile']=new_rsp_fn
+        ht['clobber']="yes"
+        ht.run()
 
 
 class SpectraBins(ddosa.SpectraBins):
